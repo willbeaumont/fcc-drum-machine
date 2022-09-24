b5b4f2656b87576adf660019e1678c13afce8aa6
Will Beaumont <39424606+nmbeau@users.noreply.github.com>
2022-09-24 15:41:38 -0700

    complete user stories

--- src/App.js
+++ src/App.js
@@ -1,49 +1,128 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './index.css';
 
-const name = "clip_name"
-const test = "test1"
+const drumKeys = [
+  {
+    name: "q",
+    audio: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    description: "Heater 1"
+  },
+  {
+    name: "w",
+    audio: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    description: "Heater 2"
+  },
+  {
+    name: "e",
+    audio: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    description: "Heater 3"
+  },
+  {
+    name: "a",
+    audio: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    description: "Heater 4"
+  },
+  {
+    name: "s",
+    audio: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    description: "Clap"
+  },
+  {
+    name: "d",
+    audio: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    description: "Open-HH"
+  },
+  {
+    name: "z",
+    audio: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    description: "Closed-HH"
+  },
+  {
+    name: "x",
+    audio: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    description: "Kick-n'-Hat"
+  },
+  {
+    name: "c",
+    audio: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    description: "Kick"
+  }
+]
 
-const drumKeys = ["q", "w", "e", "a", "s", "d", "z", "x", "c"]
+function hitIt(btn, updateName, name) {
+  updateName(name);
+  btn.pause();
+  btn.currentTime = 0;
+  btn.play();
+};
 
 function DrumPad(props) {
+  const ref = useRef(null);
+
+  const playKey = () => {
+    hitIt(ref.current, props.setKeyName, props.drumKey.description);
+  }
+
   return (
-    <div>
-      <button id={props.clipName} type="button">{props.buttonKey}</button>
-    </div>
+    <button
+      className="drum-pad bg-slate-300 w-20 h-20"
+      id={props.drumKey.description}
+      type="button"
+      onClick={playKey}>
+      {props.drumKey.name.toUpperCase()}
+      <audio ref={ref} className="clip" id={props.drumKey.name.toUpperCase()} src={props.drumKey.audio} desc={props.drumKey.description} />
+    </button>
    );
 }
 
-function Drums() {
+function Drums(props) {
   return (
-    <div className="bg-slate-600">
-      <DrumPad clipName={name} buttonKey={test} />
+    <div className="grid grid-cols-3 gap-4">
+      {drumKeys.map(drumKey => {
+        return <DrumPad drumKey={drumKey} key={drumKey.name} setKeyName={props.setKeyName} />;
+      })}
     </div>
   );
 }
 
-function Display() {
+function Display(props) {
   return (
-    <div id="display"></div>
+    <div>
+      <div id="display" className="bg-white">
+        {props.keyName}
+      </div>
+    </div>
   );
 }
 
 
-
-function DrumMachine() {
+function DrumMachine(props) {
   return (
-    <div id="drum-machine" className="flex flex-row mx-auto items-center lg:flex-row">
-      <DrumPad clipName={name} buttonKey={test} />
-      <Drums />
-      <Display />
+    <div id="drum-machine" className="bg-slate-600 p-4 m-auto flex flex-row">
+      <Drums setKeyName={props.setKeyName} />
+      <Display keyName={props.keyName} className="w-70 h-70"/>
     </div>
   );
 }
 
 function App() {
+  const [keyName, setKeyName] = useState("");
+  const handleKeyDown = (e) => {
+    if (drumKeys.map(k => k.name).includes(e.key.toLowerCase())) {
+      const btn = document.getElementById(e.key.toUpperCase())
+      hitIt(btn, setKeyName, btn.getAttribute("desc"));
+    };
+  };
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    ref.current.focus();
+  }, []);
+
   return (
-    <div className="bg-slate-200 h-screen w-screen flex">
-      <DrumMachine className="bg-slate-500 w-6 h-6" clipName={name} buttonKey={test} />
+    <div ref={ref} tabIndex={-1} onKeyDown={handleKeyDown} className="bg-slate-200 h-screen w-screen flex">
+      <DrumMachine className="bg-slate-500 flex" keyName={keyName} setKeyName={setKeyName} />
     </div>
   );
 }
