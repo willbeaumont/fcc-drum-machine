1d8d0c7105a0c670687daf67b153101255f9627f
Will Beaumont <39424606+nmbeau@users.noreply.github.com>
2023-09-08 08:34:28 -0700

    add build folder, tailwind config, and app features

--- src/App.js
+++ src/App.js
@@ -49,37 +49,43 @@ const drumKeys = [
   }
 ]
 
-function hitIt(btn, updateName, name) {
+function hitIt(btn, updateName, name, volume) {
   updateName(name);
-  btn.pause();
   btn.currentTime = 0;
   btn.play();
+  btn.volume = volume;
 };
 
 function DrumPad(props) {
   const ref = useRef(null);
 
   const playKey = () => {
-    hitIt(ref.current, props.setKeyName, props.drumKey.description);
+    hitIt(ref.current, props.setKeyName, props.drumKey.description, props.volume);
   }
 
   return (
     <button
-      className="drum-pad bg-slate-300 w-20 h-20"
+      className="drum-pad bg-teal-500 hover:bg-teal-600 w-16 h-16 border border-sky-900 rounded-md"
       id={props.drumKey.description}
       type="button"
       onClick={playKey}>
       {props.drumKey.name.toUpperCase()}
-      <audio ref={ref} className="clip" id={props.drumKey.name.toUpperCase()} src={props.drumKey.audio} desc={props.drumKey.description} />
+      <audio
+        ref={ref}
+        className="clip"
+        id={props.drumKey.name.toUpperCase()}
+        src={props.drumKey.audio}
+        desc={props.drumKey.description}
+      />
     </button>
    );
 }
 
 function Drums(props) {
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="bg-teal-300 w-72 h-72 grid grid-cols-3 place-items-center">
       {drumKeys.map(drumKey => {
-        return <DrumPad drumKey={drumKey} key={drumKey.name} setKeyName={props.setKeyName} />;
+        return <DrumPad drumKey={drumKey} key={drumKey.name} volume={props.volume} setKeyName={props.setKeyName} />;
       })}
     </div>
   );
@@ -87,20 +93,34 @@ function Drums(props) {
 
 function Display(props) {
   return (
-    <div>
-      <div id="display" className="bg-white">
-        {props.keyName}
+    <div id="display" className="bg-teal-400 w-72 h-72 py-4 flex flex-col items-center place-content-around">
+      <h1 className="text-3xl">Drum Machine</h1>
+      <div className="flex flex-col items-center">
+        <h2 className="text-xl p-1">Sound Name</h2>
+        <div className="bg-teal-100 w-full h-7 text-center border border-sky-900 rounded-md">
+          <p className="text-lg">{props.keyName}</p>
+        </div>
+      </div>
+      <div className="flex flex-col items-center">
+        <h2 className="text-xl p-1">Volume</h2>
+        <div>
+          <input
+            type="range"
+            min="0" max="1" step="0.01"
+            value={props.volume}
+            onChange={(e) => props.setVolume(e.target.value)}
+            className="w-full h-2 bg-gray-300 appearance-none cursor-pointer accent-sky-900" />
+        </div>
       </div>
     </div>
   );
 }
 
-
 function DrumMachine(props) {
   return (
-    <div id="drum-machine" className="bg-slate-600 p-4 m-auto flex flex-row">
-      <Drums setKeyName={props.setKeyName} />
-      <Display keyName={props.keyName} className="w-70 h-70"/>
+    <div id="drum-machine" className="m-auto border-4 border-sky-900 divide-y-4 md:divide-x-4 md:divide-y-0 divide-sky-900 rounded-md flex flex-col md:flex-row">
+      <Display keyName={props.keyName} volume={props.volume} setVolume={props.setVolume} />
+      <Drums setKeyName={props.setKeyName} volume={props.volume} />
     </div>
   );
 }
@@ -110,19 +130,21 @@ function App() {
   const handleKeyDown = (e) => {
     if (drumKeys.map(k => k.name).includes(e.key.toLowerCase())) {
       const btn = document.getElementById(e.key.toUpperCase())
-      hitIt(btn, setKeyName, btn.getAttribute("desc"));
+      hitIt(btn, setKeyName, btn.getAttribute("desc"), volume);
     };
   };
+
+  const [volume, setVolume] = useState(1);
 
   const ref = useRef(null);
 
   useEffect(() => {
     ref.current.focus();
-  }, []);
+  });
 
   return (
-    <div ref={ref} tabIndex={-1} onKeyDown={handleKeyDown} className="bg-slate-200 h-screen w-screen flex">
-      <DrumMachine className="bg-slate-500 flex" keyName={keyName} setKeyName={setKeyName} />
+    <div ref={ref} tabIndex={-1} onKeyDown={handleKeyDown} className="bg-teal-100 w-screen h-screen flex text-sky-900">
+      <DrumMachine keyName={keyName} setKeyName={setKeyName} volume={volume} setVolume={setVolume} />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-   "./src/**/*.{js,jsx,ts,tsx}",
+   './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
+//      colors: {
+//        'primaryColor': '3AB795',
+//        'secondaryColor': 'A0E8AF',
+//        'textColor': '4D4730',
+//      },
     extend: {},
   },
   plugins: [],
